274589e746fe0e723a1b748bd35024121eccf148
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-01 19:30:16 -0300

    Adicionando um tratamento global para os erros da aplicação

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,1 +1,9 @@
 import { Router } from 'express';
+
+const routes = Router();
+
+routes.use('/users', () => {
+  console.log('teste');
+});
+
+export default routes;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,31 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 const app = express();
+
+app.use((req, res, next) => {
+  next();
+}, cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333);
