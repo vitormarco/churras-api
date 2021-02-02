ce04e2d82868fbb2e04954038262c1ffb14e8188
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-01 21:28:49 -0300

    Realizando o cadastro do usuário, preparando os fakes para implementar os testes

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import User from '../infra/typeorm/entities/User';
+import IUserRepository from '../repositories/IUserRepository';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) { }
+
+  public async execute({ name, email, password }: IUserRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) throw new AppError('Email already exists.');
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+
 const routes = Router();
 
-routes.use('/users', () => {
-  console.log('teste');
-});
+routes.use('/users', usersRouter);
 
 export default routes;
